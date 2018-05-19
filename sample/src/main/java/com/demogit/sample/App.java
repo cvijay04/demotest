d1ead6ff8ed530ef1ba806b16854a540b751cdf6
Vijay <cr.vijay.kumar@gmail.com>
2018-05-19 13:31:42 +0530

    Test initial commit
    Signed-off-by: Vj

---
+++ sample/src/main/java/com/demogit/sample/App.java
@@ -0,0 +1,15 @@
+package com.demogit.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Sample Test!" );
+        
+    }
+}
