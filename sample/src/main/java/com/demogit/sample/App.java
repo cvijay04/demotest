b70c3de8ee0935a23560615a1b3d9e1cc0278e10
Vijay <cr.vijay.kumar@gmail.com>
2018-05-19 23:08:24 +0530

    updating pom.xml

--- sample/src/main/java/com/demogit/sample/App.java
+++ sample/src/main/java/com/demogit/sample/App.java
@@ -1,17 +1,29 @@
 package com.demogit.sample;
 
-/**
- * Hello world!
- *
- */
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 public class App 
 {
-    public static void main( String[] args )
+		
+		public WebDriver driver;
+
+	  @Test
+	  public void openMyBlog() {
+		  System.setProperty("webdriver.gecko.driver", "D:/Drivers/geckodriver.exe");
+		  driver = new FirefoxDriver();
+		  driver.get("https://www.softwaretestingmaterial.com/");
+	  }
+	  
+
+
+    /*public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         System.out.println( "learning git starting" );
         
         
         
-    }
+    }*/
 }
