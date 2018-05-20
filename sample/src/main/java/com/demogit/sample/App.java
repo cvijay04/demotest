69252030afa775e16b9082618829bf33d7e99d6d
Vijay <cr.vijay.kumar@gmail.com>
2018-05-20 20:52:26 +0530

    update driverfile,app file and datasheet

--- sample/src/main/java/com/demogit/sample/App.java
+++ sample/src/main/java/com/demogit/sample/App.java
@@ -1,5 +1,13 @@
 package com.demogit.sample;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
@@ -11,10 +19,42 @@ public class App
 
 	  @Test
 	  public void openMyBlog() {
-		  System.setProperty("webdriver.gecko.driver", "D:/Drivers/geckodriver.exe");
+		  System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
 		  driver = new FirefoxDriver();
-		  driver.get("https://www.softwaretestingmaterial.com/");
-	  }
+		  //driver.get("https://www.softwaretestingmaterial.com/");
+		  
+		  try {
+	            FileInputStream file = new FileInputStream(new File("src/main/resources/Datasheet.xlsx"));
+	 
+	            // Create Workbook instance holding reference to .xlsx file
+	            XSSFWorkbook workbook = new XSSFWorkbook(file);
+	 
+	            // Get first/desired sheet from the workbook
+	            XSSFSheet sheet = workbook.getSheetAt(0);
+	 
+	            // Iterate through each rows one by one
+	            Iterator<Row> rowIterator = sheet.iterator();
+	            while (rowIterator.hasNext()) {
+	                Row row = rowIterator.next();
+	                // For each row, iterate through all the columns
+	                Iterator<Cell> cellIterator = row.cellIterator();
+	 
+	                while (cellIterator.hasNext()) {
+	                    Cell cell = cellIterator.next();
+	                    driver.get(cell.getStringCellValue());
+	                    
+	                }
+	                System.out.println("");
+	            }
+	            file.close();
+	        }
+	        catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+		  
+		  
+	  
 	  
 
 
